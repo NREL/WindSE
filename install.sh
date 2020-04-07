b0280ff87f159274a3679c597c39feb2c7324a47
Jeff Allen <jeffery.allen@nrel.gov>
2020-04-07 10:13:25 -0600

    travis yml testing using install.sh part 5

--- install.sh
+++ install.sh
@@ -11,8 +11,7 @@ conda activate $1
 ### Install conda-forge dependencies 
 conda install -y -c conda-forge fenics=2019.1.0 dolfin-adjoint matplotlib scipy mshr hdf5 pyyaml memory_profiler pytest
 
-### Install editible version of WindSE
-pip install -e .
+echo "INSTALL COMPLETE NO IM PIPIN'"
 
 ### Install the new tsfc compiler
 pip install git+https://github.com/blechta/tsfc.git@2018.1.0
@@ -20,3 +19,5 @@ pip install git+https://github.com/blechta/COFFEE.git@2018.1.0
 pip install git+https://github.com/blechta/FInAT.git@2018.1.0
 pip install singledispatch networkx pulp
 
+### Install editible version of WindSE
+pip install -e .
