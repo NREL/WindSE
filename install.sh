c68a444b5b6f4d7489b5a692e11f65c40da1513b
Jeff Allen <jeffery.allen@nrel.gov>
2020-04-07 10:51:28 -0600

    install.sh test 8

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 ### Run this to use conda in the script
 # eval "$(conda shell.bash hook)"
+echo "This is the location of miniconda"
+conda info --base
+echo ""
 source $(conda info --base)/etc/profile.d/conda.sh
 
 ### Create the Environment
