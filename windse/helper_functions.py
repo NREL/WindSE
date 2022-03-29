88de0ff46d70208dfd51a9fe8582efd10fe556e0
Ethan Young <ethan.young@nrel.gov>
2022-03-29 16:13:28 -0600

    adding code for mpi communication of point evaluations

--- windse/helper_functions.py
+++ windse/helper_functions.py
@@ -8,36 +8,73 @@ if windse_parameters.dolfin_adjoint:
     from windse.blocks import blockify, MpiEvalBlock
 
 def mpi_eval(u, x, comm=MPI.comm_world):
-    # dim = u.function_space().dim()
-    # mesh = u.function_space().mesh
 
-    # # only evaluate on the processor that owns the point
-    # val = np.nan
-    # point = Point(x)
-    # if mesh.bounding_box_tree().compute_first_entity_collision(point) < mesh.num_cells():
-    #     val = u(point)
-    #     rank = comm.rank
+    '''
+
+    This function returns the value of a function, ``u``, at a point, ``x``. 
+    It is written to carry out this operation in parallel where not all
+    processes will have ownership of the point ``x``. In this case, ownership 
+    is established with ``bounding_box_tree().compute_first_entity_collision(point)``
+    where the resulting interpolated value is then distributed to all processes 
+    immediately afterward.
+    
+    Args:
+        u (dolfin function):
+            The dolfin function to be sampled. 
+        x ([1x2], [1x3] list):
+            The point at which the function should be sampled, must be within the domain of ``u``. 
+
+    Returns:
+        Constant(ux):
+            The interpolated value of function ``u`` at point ``x`` as a dolfin Constant. 
+
+    '''
+
+    rank = comm.Get_rank()
+    num_procs = comm.Get_size()
+
+    value_rank = u.value_rank()
+    mesh = u.function_space().mesh()
+    u.set_allow_extrapolation(False)
+
+    point = Point(x)
+
+    if value_rank == 0:
+        # For a scalar function, create a single placeholder
+        ux = np.array(np.nan)
+        nn = 1
+
+    elif value_rank == 1:
+        # For a vector function, create a placeholder with ndim entries
+        ux = np.array([np.nan for k in range(u.geometric_dimension())])
+        nn = len(ux)
+
+    ux_global = np.zeros(nn*num_procs)
+
+    # Check if this rank owns the point and, if so, evaluate the function there 
+    if mesh.bounding_box_tree().compute_first_entity_collision(point) < mesh.num_cells():
+        ux = np.array(u(point))
 
 
+    # implementation_method = 1
+    implementation_method = 2 # 2 is recommended, type(ux) is variable with method 1
 
+    if implementation_method == 1:
 
+        comm.Gather(ux, ux_global, root=0)
 
+        if rank == 0:
+            ux_global = ux_global.reshape(-1, nn)
+            ux = np.nanmean(ux_global, axis=0)
 
+        comm.Bcast(ux, root=0)
 
+    elif implementation_method == 2:
 
-    u.set_allow_extrapolation(False) # use to make sure we don't evaluate outside of a processor
+        comm.Allgather(ux, ux_global)
 
-    try:
-        ux = u(np.array(x))
-    except:
-        ux = np.nan
-
-    u_list = comm.allgather(ux)
-
-    for val in u_list:
-        if not np.isnan(val):
-            ux = val
-            break
+        ux_global = ux_global.reshape(-1, nn)
+        ux = np.nanmean(ux_global, axis=0)
 
     return Constant(ux)
 
