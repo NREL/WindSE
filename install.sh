4af71c24bd33874ef181d9c503b8dd05d087c48c
John Jasa <johnjasa11@gmail.com>
2022-04-05 13:26:18 -0600

    Pin OM version until bug resolved

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ pip install git+https://github.com/blechta/tsfc.git@2018.1.0
 pip install git+https://github.com/blechta/COFFEE.git@2018.1.0
 pip install git+https://github.com/blechta/FInAT.git@2018.1.0
 pip install git+https://github.com/mdolab/pyoptsparse@v1.0
-pip install singledispatch networkx pulp openmdao
+pip install singledispatch networkx pulp openmdao==3.16.0
 
 ### Install editible version of WindSE
 pip install -e .
