dee55cbb85eff70fe51477a8fc18a211fa77e69c
Young <eyoung55@gmail.com>
2023-06-27 11:35:26 -0600

    make inputs to all refinements kwargs for unpacking from dictionary

--- windse/RefinementManager.py
+++ windse/RefinementManager.py
@@ -29,7 +29,11 @@ def CreateRefinementList(dom, farm, refine_params):
             if farm_type == 'box':
                 RD = max(RDs)
                 bbox[2] = [bbox[2][0]-RD,bbox[2][1]+RD]
-                refine_list.append(["box",[bbox,expand_factor]])
+                # refine_list.append(["box",[bbox,expand_factor]])
+                refine_list.append({"type": "box",
+                                    "x_range": bbox[0],
+                                    "y_range": bbox[1],
+                                    "z_range": bbox[2]})
 
             elif farm_type == 'cylinder':
                 RD = max(RDs)
@@ -43,7 +47,12 @@ def CreateRefinementList(dom, farm, refine_params):
                     center = [x0,y0]
                     height = 0
                 radius = np.sqrt((bbox[0][1]-bbox[0][0])**2+(bbox[1][1]-bbox[1][0])**2)/2.0
-                refine_list.append(["cylinder",[center,radius,height,expand_factor]])
+                # refine_list.append(["cylinder",[center,radius,height,expand_factor]])
+                refine_list.append({"type": "cylinder",
+                                    "center": center,
+                                    "radius": radius,
+                                    "height": height,
+                                    "expand_factor": expand_factor})
 
             elif farm_type == 'stream':
                 RD = max(RDs)
@@ -59,7 +68,14 @@ def CreateRefinementList(dom, farm, refine_params):
                 length = bbox[0][1]-bbox[0][0]+6*RD
                 theta = dom.inflow_angle
                 pivot_offset = 3*max(RDs)/2.0
-                refine_list.append(["stream",[center,radius,length,theta,pivot_offset,expand_factor]])
+                # refine_list.append(["stream",[center,radius,length,theta,pivot_offset,expand_factor]])
+                refine_list.append({"type": "stream",
+                                    "center": center,
+                                    "radius": radius,
+                                    "length": length,
+                                    "theta": theta,
+                                    "pivot_offset": pivot_offset,
+                                    "expand_factor": expand_factor})
 
     if turbine_num > 0 and farm.numturbs > 0:
         for i in range(turbine_num,0,-1):
@@ -67,32 +83,56 @@ def CreateRefinementList(dom, farm, refine_params):
 
             if turbine_type == 'simple':
                 radius = max(RDs)
-                refine_list.append(["simple",[radius,expand_factor]])
+                # refine_list.append(["simple",[radius,expand_factor]])
+                refine_list.append({"type": "simple",
+                                    "radius": radius,
+                                    "expand_factor": expand_factor})
 
             elif turbine_type == 'sphere':
                 radius = max(RDs)
-                refine_list.append(["sphere",[radius,expand_factor]])
+                # refine_list.append(["sphere",[radius,expand_factor]])
+                refine_list.append({"type": "sphere",
+                                    "radius": radius,
+                                    "expand_factor": expand_factor})
 
             elif turbine_type == 'wake':
                 radius = max(RDs)
                 length = 5*radius
                 theta = dom.inflow_angle
-                refine_list.append(["wake",[radius,length,theta,expand_factor]])
+                # refine_list.append(["wake",[radius,length,theta,expand_factor]])
+                refine_list.append({"type": "wake",
+                                    "radius": radius,
+                                    "length": length,
+                                    "theta": theta,
+                                    "expand_factor": expand_factor})
 
             elif turbine_type == 'tear':
                 radius = max(RDs)
                 theta = dom.inflow_angle
-                refine_list.append(["tear",[radius,theta,expand_factor]])
+                # refine_list.append(["tear",[radius,theta,expand_factor]])
+                refine_list.append({"type": "tear",
+                                    "radius": radius,
+                                    "theta": theta,
+                                    "expand_factor": expand_factor})
 
         if refine_power_calc:
             radius = max(RDs)
             length = radius/5.0
             theta = dom.inflow_angle
             centered = True
-            refine_list.append(["wake",[radius,length,theta,expand_factor,centered]])
+            # refine_list.append(["wake",[radius,length,theta,expand_factor,centered]])
+            refine_list.append({"type": "wake",
+                                "radius": radius,
+                                "length": length,
+                                "theta": theta,
+                                "expand_factor": expand_factor,
+                                "centered": centered})
 
+    if refine_custom is not None:
+        sorted_refine_custom_keys = sorted(list(refine_custom.keys()))
 
-
+        for key in sorted_refine_custom_keys:
+            refine_list.append(refine_custom[key])
 
     return refine_list
 
@@ -118,14 +158,16 @@ def RefineMesh(dom,farm):
 
     ### Step through refine instructions ###
     num = len(refine_list)
-    for i, refine_step in enumerate(refine_list):
+    for i, refinement_dict in enumerate(refine_list):
         fprint("Refining Mesh Step {:d} of {:d}".format(i+1,num), special="header")
         step_start = time.time()
         
-        refine_type = refine_step[0]
-        refine_args = refine_step[1]
+        refine_type = refinement_dict["type"]
+        refine_args = dict(refinement_dict)
+        del refine_args["type"]
+
         refine_func = refine_dict[refine_type]
-        refine_func(*refine_args)
+        refine_func(**refine_args)
 
         step_stop = time.time()
         fprint("Step {:d} of {:d} Finished: {:1.2f} s".format(i+1,num,step_stop-step_start), special="footer")
